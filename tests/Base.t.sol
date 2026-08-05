dbf35aae20ef4e380e6bcc74d2b47831ecddbc59
ammarsjw <ammarsjw@gmail.com>
2026-08-05 11:42:24 +0500

    move: psm to reserve

--- tests/Base.t.sol
+++ tests/Base.t.sol
@@ -6,21 +6,21 @@ import { Test } from "forge-std/Test.sol";
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
+import { CollateralAdapter } from "../contracts/core/CollateralAdapter.sol";
 import { USDR } from "../contracts/core/USDR.sol";
 import { VaultEngine } from "../contracts/core/VaultEngine.sol";
-import { CollateralAdapter } from "../contracts/core/CollateralAdapter.sol";
-import { OracleSecurityModule } from "../contracts/oracle/OracleSecurityModule.sol";
-import { PriceConverter } from "../contracts/oracle/PriceConverter.sol";
-import { PegStabilityModule } from "../contracts/psm/PegStabilityModule.sol";
-import { ReserveAccounting } from "../contracts/reserve/ReserveAccounting.sol";
-import { SolvencyEngine } from "../contracts/reserve/SolvencyEngine.sol";
-import { BalanceSheet } from "../contracts/reserve/BalanceSheet.sol";
-import { PriceCurve } from "../contracts/liquidation/PriceCurve.sol";
-import { LiquidationTrigger } from "../contracts/liquidation/LiquidationTrigger.sol";
-import { DutchAuction } from "../contracts/liquidation/DutchAuction.sol";
-import { CircuitBreaker } from "../contracts/liquidation/CircuitBreaker.sol";
 import { Governor } from "../contracts/governance/Governor.sol";
 import { IPriceSource } from "../contracts/interfaces/IPriceSource.sol";
+import { CircuitBreaker } from "../contracts/liquidation/CircuitBreaker.sol";
+import { DutchAuction } from "../contracts/liquidation/DutchAuction.sol";
+import { LiquidationTrigger } from "../contracts/liquidation/LiquidationTrigger.sol";
+import { PriceCurve } from "../contracts/liquidation/PriceCurve.sol";
+import { OracleSecurityModule } from "../contracts/oracle/OracleSecurityModule.sol";
+import { PriceConverter } from "../contracts/oracle/PriceConverter.sol";
+import { BalanceSheet } from "../contracts/reserve/BalanceSheet.sol";
+import { PegStabilityModule } from "../contracts/reserve/PegStabilityModule.sol";
+import { ReserveAccounting } from "../contracts/reserve/ReserveAccounting.sol";
+import { SolvencyEngine } from "../contracts/reserve/SolvencyEngine.sol";
 import { _RAD, _RAY, _USDR_ILK, _WAD, _WARD_ROLE } from "../contracts/shared/Constants.sol";
 
 import { MockERC20 } from "./mocks/MockERC20.sol";
